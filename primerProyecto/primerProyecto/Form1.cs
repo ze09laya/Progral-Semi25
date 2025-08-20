a24213e7b919792c254419d06552277296dd8363
Michelle Zelaya <michellecastro2122@gmail.com>
2025-08-19 19:24:57 -0600

    Se agrego conversores de moneda, masa, volumen, longitud, tiempo y almacenamiento

--- primerProyecto/primerProyecto/Form1.cs
+++ primerProyecto/primerProyecto/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -32,57 +33,117 @@ namespace primerProyecto
 
         }
 
-        
+
         private void cboOpciones_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
 
-        private double[][] tablaIsr = {
-            new Double[] {0.01, 550, 0, 0},
-            new Double[] {550.01, 895.24, 0.10, 17.67},
-            new Double[] {895.25, 2038.10, 0.20, 60},
-            new Double[] {2038.11, 9999999, 0.30, 288.57}
-
-
-            };
-
-        private double calcularDeducciones(double sueldo, double porcentaje)
+        private void btnCalcular_Click(object sender, EventArgs e)
         {
-            return sueldo * porcentaje;
+            double cantidad = double.Parse(txtCantidad.Text);
+            double resultado = 0;
+
+            if (optDolar.Checked) resultado = cantidad * 1.00;
+            if (optEuro.Checked) resultado = cantidad * 0.86;
+            if (optMexico.Checked) resultado = cantidad * 18.84;
+            if (optColonsalvadoreño.Checked) resultado = cantidad * 8.75;
+            if (optQuetzal.Checked) resultado = cantidad * 7.67;
+            if (optLempira.Checked) resultado = cantidad * 26.30;
+            if (optCordoba.Checked) resultado = cantidad * 36.80;
+            if (optColoncostarricense.Checked) resultado = cantidad * 505.16;
+            if (optBalboa.Checked) resultado = cantidad * 1.00;
+            if (optColombia.Checked) resultado = cantidad * 4033.63;
+
+            if (optGramo.Checked) resultado = cantidad * 1;
+            if (optKilogramo.Checked) resultado = cantidad * 1000;
+            if (optMiligramo.Checked) resultado = cantidad / 1000;
+            if (optTonelada.Checked) resultado = cantidad * 100000;
+            if (optLibra.Checked) resultado = cantidad * 453.592;
+            if (optOnza.Checked) resultado = cantidad * 28.35;
+            if (optQuintal.Checked) resultado = cantidad * 100000;
+            if (optStone.Checked) resultado = cantidad * 6350;
+            if (optMicrogramo.Checked) resultado = cantidad * 1000000;
+            if (optCarat.Checked) resultado = cantidad * 200;
+
+            if (optLitro.Checked) resultado = cantidad * 1;
+            if (optMililitro.Checked) resultado = cantidad * 1000;
+            if (optMetrocúbico.Checked) resultado = cantidad * 100;
+            if (optCentímetrocúbico.Checked) resultado = cantidad * 1;
+            if (optGalón.Checked) resultado = cantidad * 3.785;
+            if (optPinta.Checked) resultado = cantidad * 0.473;
+            if (optOnzalíquida.Checked) resultado = cantidad * 0.029;
+            if (optBarril.Checked) resultado = cantidad * 159;
+            if (optDecilitro.Checked) resultado = cantidad * 10;
+            if (optCucharada.Checked) resultado = cantidad * 0.015;
+            lblResultado.Text = "Respuesta: " + resultado;
+
         }
 
-        private double calcularIsr(double sueldo)
+        private void btnRespuesta_Click(object sender, EventArgs e)
         {
-            double isr = 0;
-            for (int i = 0; i < tablaIsr.Length; i++)
+            try
             {
-                if (sueldo >= tablaIsr[i][0] && sueldo <= tablaIsr[i][1])
+                double cantidad = double.Parse(txtCantidad.Text);
+                double resultado = 0;
+
+    
+                if (cboLongitud.SelectedItem != null)
                 {
-                    isr = (sueldo - tablaIsr[i][0]) * tablaIsr[i][2] + tablaIsr[i][3];
-
+                    switch (cboLongitud.SelectedItem.ToString())
+                    {
+                        case "Metro": resultado = cantidad * 1; break;
+                        case "Kilómetro": resultado = cantidad / 1000; break;
+                        case "Centímetro": resultado = cantidad * 100; break;
+                        case "Milímetro": resultado = cantidad * 1000; break;
+                        case "Micrómetro": resultado = cantidad * 1e6; break;
+                        case "Nanómetro": resultado = cantidad * 1e9; break;
+                        case "Pulgada": resultado = cantidad * 39.37; break;
+                        case "Pie": resultado = cantidad * 3.281; break;
+                        case "Yarda": resultado = cantidad * 1.094; break;
+                        case "Milla": resultado = cantidad * 0.000621; break;
+                    }
                 }
+
+                if (cboAlmacenamiento.SelectedItem != null)
+                {
+                    switch (cboAlmacenamiento.SelectedItem.ToString())
+                    {
+                        case "Bit": resultado = cantidad * 1; break;
+                        case "Byte": resultado = cantidad / 8; break;
+                        case "Kilobyte": resultado = cantidad / 8000; break;
+                        case "Megabyte": resultado = cantidad / 8e6; break;
+                        case "Gigabyte": resultado = cantidad / 8e9; break;
+                        case "Terabyte": resultado = cantidad / 8e12; break;
+                        case "Petabyte": resultado = cantidad / 8e15; break;
+                        case "Exabyte": resultado = cantidad / 8e18; break;
+                        case "Zettabyte": resultado = cantidad / 8e21; break;
+                        case "Yottabyte": resultado = cantidad / 8e24; break;
+                    }
+                }
+                if (cboTiempo.SelectedItem != null)
+                {
+                    switch (cboTiempo.SelectedItem.ToString())
+                    {
+                        case "Segundo": resultado = cantidad * 1; break;
+                        case "Minuto": resultado = cantidad / 60; break;
+                        case "Hora": resultado = cantidad / 3600; break;
+                        case "Día": resultado = cantidad / 86400; break;
+                        case "Semana": resultado = cantidad / 604800; break;
+                        case "Mes": resultado = cantidad / 2.629746e6; break;
+                        case "Año": resultado = cantidad / 3.15576e7; break;
+                        case "Década": resultado = cantidad / 3.15576e8; break;
+                        case "Siglo": resultado = cantidad / 3.15576e9; break;
+                        case "Milenio": resultado = cantidad / 3.15576e10; break;
+                    }
+                }
+
+                lblResultado.Text = resultado.ToString();
             }
-            return isr;
-        }
-      
-        private void btnCalcular_Click_1(object sender, EventArgs e)
-        {
-            double sueldo = 0, isss = 0, afp = 0, isr = 0, sueldoNeto = 0;
-            sueldo = double.Parse(txtSueldo.Text);
-
-            isss = calcularDeducciones(sueldo, 0.03); // 3% de ISSS -> 3/100=0.03
-            afp = calcularDeducciones(sueldo, 0.0725); // 7.25% de AFP -> 7.25/100=0.0725
-            isr = calcularIsr(sueldo - isss - afp); // Calcular ISR
-
-            sueldoNeto = sueldo - isss - afp - isr; // Calcular sueldo neto
-
-            lblIsss.Text = "ISSS: " + isss.ToString("C2");
-            lblAFP.Text = "AFP: " + afp.ToString("C2");
-            lblIsr.Text = "ISR: " + isr.ToString("C2");
-            lblTotalDeducciones.Text = "Total Deducciones: " + (isss + afp + isr).ToString("C2");
-            lblSueldoNeto.Text = "Sueldo Neto: " + sueldoNeto.ToString("C2");
-
+            catch
+            {
+                MessageBox.Show("Ingresa un valor numérico válido.");
+            }
         }
     }
 }
