c6379151cf639fa92e7c6bcd6eb92a7ac8869835
Michelle Zelaya <michellecastro2122@gmail.com>
2025-09-01 16:01:36 -0600

    haciendo los mismos calculos pero usando LINQ para la conversion de la matriz a entero

--- primerProyecto/primerProyecto/Form1.cs
+++ primerProyecto/primerProyecto/Form1.cs
@@ -24,47 +24,50 @@ namespace primerProyecto
         //Funciones
         //LINQ = Lenguaje de Consulta Integrado
 
-        double media(string[] serie)
-        {
-            int n = serie.Length;
-            double suma = 0;
 
-            for (int i = 0; i < n; i++)
+       
+            double media(int[] serie)
             {
-                suma += int.Parse(serie[i]);
+                int n = serie.Length;
+                double suma = 0;
+
+                for (int i = 0; i < n; i++)
+                {
+                    suma += serie[i];
+                }
+                return suma / n;
             }
-            return suma / n;
-        }
-        double tipica(string[] serie, double m)
+            
+            
+                double tipica(int[] serie, double m)
+                {
+                    double tipica = 0;
+                    int n = serie.Length;
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        
+                        tipica += Math.Pow(serie[i] - m, 2);
+                    }
+                    tipica = Math.Sqrt(tipica / n);
+                    return tipica;
+                }
+                
+                
+                    double armonica(int[] serie)
+                    {
+                        double mediaArmonica = 0;
+                        int n = serie.Length;
+                        for (int i = 0; i < n; i++)
+                        {
+                            mediaArmonica += 1 / (double)serie[i];
+                        }
+                        return n / mediaArmonica;
+                    }
+
+private void btnCalcular_Click(object sender, EventArgs e)
         {
-            double tipica = 0;
-            int n = serie.Length;
-
-            for (int i = 0; i < n; i++)
-            {
-                int num = int.Parse(serie[i]);
-                tipica += Math.Pow(num - m, 2);
-            }
-            tipica = Math.Sqrt(tipica / n);
-            return tipica;
-        }
-        double armonica(string[] serie)
-        {
-            double mediaArmonica = 0;
-            int n = serie.Length;
-            for (int i = 0; i < n; i++)
-            {
-                mediaArmonica += 1 / double.Parse(serie[i]);
-            }
-            return n / mediaArmonica;
-        }
-
-
-        private void btnCalcular_Click(object sender, EventArgs e)
-        {
-            //int[] serie = txtSerie.Text.Split(',').Select(n=>int.Parse(n)).ToArray(); //5,8,1,9 => ["5", "8", "1", "9"]
-            string[] serie = txtSerie.Text.Split(',');
-
+            int[] serie = txtSerie.Text.Split(',').Select(n => int.Parse(n)).ToArray(); //5,8,1,9 => ["5", "8", "1", "9"]
             double mediaAritmetica = media(serie);
             double desviacionTipica = tipica(serie, mediaAritmetica);
             double mediaArmonica = armonica(serie);
@@ -75,6 +78,8 @@ namespace primerProyecto
         }
     }
 }
+
+
 
 
 
