4a21d91a36c22a984a57bfe8c590f1373ea64180
Michelle Zelaya <michellecastro2122@gmail.com>
2025-09-13 16:36:58 -0600

    Creacion de la nueva rama academica, para computo II, POO y Bases de Datos

--- primerProyecto/primerProyecto/Form1.cs
+++ primerProyecto/primerProyecto/Form1.cs
@@ -19,38 +19,6 @@ namespace primerProyecto
         }
 
 
-
-        //Matrices
-        //Funciones
-        //LINQ = Lenguaje de Consulta Integrado
-
-
-
-        double media(int[] serie)
-        {
-            return serie.Average();
-        }
-        double tipica(int[] serie, double m)
-        {
-            return Math.Sqrt(serie.Average(n => Math.Pow(n - m, 2)));
-        }
-        double armonica(int[] serie)
-        {
-            int n = serie.Length;
-            return n / serie.Sum(num => 1.0 / num);
-        }
-
-        private void btnCalcular_Click(object sender, EventArgs e)
-        {
-            int[] serie = txtSerie.Text.Split(',').Select(n => int.Parse(n)).ToArray(); //5,8,1,9 => ["5", "8", "1", "9"]
-
-            double mediaAritmetica = media(serie);
-            double desviacionTipica = tipica(serie, mediaAritmetica);
-            double mediaArmonica = armonica(serie);
-
-            lblMedia.Text = "Media: " + mediaAritmetica.ToString();
-            lblTipica.Text = "Tipica: " + desviacionTipica.ToString();
-            lblArmonica.Text = "Armonica: " + mediaArmonica.ToString();
         }
     }
 }
