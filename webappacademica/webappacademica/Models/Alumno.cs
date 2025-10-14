fdeb6a019d5de51b242651ff2c4bc19d09325f98
Michelle Zelaya <michellecastro2122@gmail.com>
2025-10-14 17:34:10 -0600

    Creacion de la rama webapp y del proyecto web app academica, configuracion de la clase MyDbContext y Alumno y la configuracion de la cadena de conexion a la BD

---
+++ webappacademica/webappacademica/Models/Alumno.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace webappacademica.Models
+{
+    public class Alumno {
+        [Key]
+        public int idAlumno { get; set; }
+        public string codigo { get; set; }
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+    }
+}
---
+++ webappacademica/webappacademica/Models/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+namespace webappacademica.Models
+{
+    public class MyDbContext : DbContext {
+
+        public MyDbContext() { }
+        public MyDbContext(DbContextOptions<MyDbContext> options): base(options) { }
+
+        public DbSet<Alumno> Alumnos { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Alumno>().HasKey(a => a.idAlumno);
+        }
+    }
+}
