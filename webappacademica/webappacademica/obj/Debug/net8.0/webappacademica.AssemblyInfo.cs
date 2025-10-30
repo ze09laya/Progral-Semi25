af2f176631cfdfce141a26e98447aec7ec963ef2
Michelle Zelaya <michellecastro2122@gmail.com>
2025-10-30 15:42:06 -0600

    c

--- webappacademica/webappacademica/obj/Debug/net8.0/webappacademica.AssemblyInfo.cs
+++ webappacademica/webappacademica/obj/Debug/net8.0/webappacademica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webappacademica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96f9d0bfa6317a39a08b741b9231f82e97b40852")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e55103368702c86f7876b92b7e0089ebf156499a")]
 [assembly: System.Reflection.AssemblyProductAttribute("webappacademica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webappacademica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
